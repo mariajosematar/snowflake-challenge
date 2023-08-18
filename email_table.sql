f3696f5994643db9f2e638f2673cb2216d6d2e23
María Jose <80407624+mariajosematar@users.noreply.github.com>
2023-08-18 15:19:04 +0200

    Add files via upload

---
+++ email_table.sql
@@ -0,0 +1,49 @@
+-- Crear la tabla email_table
+CREATE OR REPLACE TABLE public.email_table (
+    join_id INT,
+    email STRING
+);
+
+-- Insertar datos en la tabla email_table
+INSERT INTO public.email_table (join_id,email)
+VALUES
+(1,'1867945546@theinformationlab.nl'),
+(2,'1867945540@theinformationlab.nl'),
+(3,'1867945541@theinformationlab.nl'),
+(4,'1867945542@theinformationlab.nl'),
+(5,'1867945543@theinformationlab.nl'),
+(6,'1867945544@theinformationlab.nl'),
+(7,'1867945545@theinformationlab.nl'),
+(8,'1867945547@theinformationlab.nl'),
+(9,'1867945548@theinformationlab.nl'),
+(10,'1867945549@theinformationlab.nl'),
+(11,'1867945550@theinformationlab.nl'),
+(12,'1867945551@theinformationlab.nl'),
+(13,'1867945552@theinformationlab.nl'),
+(14,'1867945553@theinformationlab.nl'),
+(15,'1867945554@theinformationlab.nl'),
+(16,'1867945555@theinformationlab.nl'),
+(17,'1867945556@theinformationlab.nl'),
+(18,'1867945557@theinformationlab.nl'),
+(19,'1867945558@theinformationlab.nl'),
+(20,'1867945559@theinformationlab.nl'),
+(21,'1867945560@theinformationlab.nl'),
+(22,'1867945561@theinformationlab.nl'),
+(23,'1867945562@theinformationlab.nl'),
+(24,'1867945563@theinformationlab.nl'),
+(25,'1867945564@theinformationlab.nl'),
+(26,'1867945565@theinformationlab.nl'),
+(27,'1867945566@theinformationlab.nl'),
+(28,'1867945567@theinformationlab.nl'),
+(29,'1867945568@theinformationlab.nl'),
+(30,'1867945569@theinformationlab.nl'),
+(31,'1867945570@theinformationlab.nl'),
+(32,'1867945571@theinformationlab.nl'),
+(33,'1867945572@theinformationlab.nl'),
+(34,'1867945573@theinformationlab.nl'),
+(35,'1867945574@theinformationlab.nl'),
+(36,'1867945575@theinformationlab.nl'),
+(37,'1867945576@theinformationlab.nl'),
+(38,'1867945577@theinformationlab.nl'),
+(39,'1867945578@theinformationlab.nl'),
+(40,'1867945579@theinformationlab.nl');
